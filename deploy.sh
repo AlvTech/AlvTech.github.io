39e6e395fae9cd248bbfb0e811419eb1eba0d9a9
adi-ads <chikara.aditya@gmail.com>
2015-01-22 22:15:08 +0530

    work

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
  git config push.default simple
  git add --all .
  git commit -m "Compile & Deploy"
- git push --quiet "https://${GH_TOKEN}:@github.com/AlvTech/AlvTech.github.io.git"
+ git push --quiet "https://${GH_TOKEN}:@github.com/AlvTech/AlvTech.github.io.git" HEAD:master
 )
