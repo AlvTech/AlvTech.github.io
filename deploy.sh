3c920c7c8ee211eb7616fe0c86bf9e867d365e43
adi-ads <chikara.aditya@gmail.com>
2015-01-22 19:56:15 +0530

    check

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@
  git config user.email "chikara.aditya@gmail.com"
  git add .
  git commit -m "Compile & Deploy"
- git push --force --quiet "https://${secure_token}@${REPO_REF}" HEAD:master > /dev/null 2>&1
+ git push --force --quiet "https://${secure_token}@${REPO_REF}" master:master > /dev/null 2>&1
 )
