831bb0a5a72e678e59a01692aaff1210fc12cef3
adi-ads <chikara.aditya@gmail.com>
2015-01-22 21:36:15 +0530

    another 2

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
  git config push.default simple
  git add --all .
  git commit -m "Compile & Deploy"
+ git branch --set-upstream master origin/master
  git push --quiet "https://${GH_TOKEN}:@github.com/AlvTech/AlvTech.github.io.git" origin master
 )
