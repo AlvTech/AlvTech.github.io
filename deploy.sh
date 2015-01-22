bef18f1c986b6742fc1601697ef3a651d73ab14e
adi-ads <chikara.aditya@gmail.com>
2015-01-22 20:48:42 +0530

    and then there was 1

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 (
- git config user.name "adi-ads"
- git config user.email "chikara.aditya@gmail.com"
  git config push.default simple
  git add --all .
  git commit -m "Compile & Deploy"
