17bdafacb42ff171c2c94c64d3999bbeb51cf5f1
adi-ads <chikara.aditya@gmail.com>
2015-01-22 20:35:57 +0530

    now

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 (
  git config user.name "adi-ads"
  git config user.email "chikara.aditya@gmail.com"
+ git config push.default simple
  git add --all .
  git commit -m "Compile & Deploy"
  git push --quiet origin master
