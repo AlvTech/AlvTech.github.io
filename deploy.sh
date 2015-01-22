a472796bdea243ab6dbb711007b3cd2086b837ff
adi-ads <chikara.aditya@gmail.com>
2015-01-22 18:36:49 +0530

    try this

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf out || exit 0;
+mkdir out;
+node build.js
+( cd out
+ git init
+ git config user.name "adi-ads"
+ git config user.email "chikara.aditya@gmail.com"
+ git add .
+ git commit -m "Compile & Deploy"
+ git push --force --quiet "https://${secure_token}@${REPO_REF}" HEAD:master > /dev/null 2>&1
+)
